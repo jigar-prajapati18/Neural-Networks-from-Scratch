1c8caf07a7bc3209653b77464919abe4d69dfec5
jigar <56577033+jigar-prajapati18@users.noreply.github.com>
2020-08-20 12:02:03 +0530

    Add files via upload

---
+++ Simple_neuron.py
@@ -0,0 +1,9 @@
+# creating simple neuron
+inputs = [1.2,5.1,2.1]
+weights = [3.1,2.1,8.1]
+
+bias = 3
+
+output = inputs[0] * weights[0] + inputs[1] * weights[1] + inputs[1] * weights[1] + bias
+
+print(output) 
